db4c9f2695a2d51ecc44157ce15c179536c336a4
Nishal Modi <raj.54669@gmail.com>
2025-11-01 15:34:34 +0530

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-# Compatibility helpers used by different app variants.
-from github_utils import load_watchlist_from_github, upload_watchlist_to_github
-from alerts import send_telegram, add_to_alert_history
-
-def load_excel_from_github():
-    return load_watchlist_from_github()
-
-def upload_to_github(uploaded_file):
-    return upload_watchlist_to_github(uploaded_file)
